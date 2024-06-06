4843ad421b521ba51acbb95aca1ce5a8d114ea08
Edson <edson3711.es@gmail.com>
2024-06-06 00:14:22 -0300

    inicio do projeto de estudo

---
+++ src/test/java/com/edson/bookstore_jpa/BookstoreJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.bookstore_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
